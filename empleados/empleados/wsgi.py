6e1683b34da3c0111a7a081f7033e32d311c746d
Aaron <acostaaaron@iresm.edu.ar>
2026-03-27 18:55:39 -0300

    correcion del settings

--- empleados/empleados/wsgi.py
+++ empleados/empleados/wsgi.py
@@ -6,6 +6,6 @@ sys.path.insert(0, BASE_DIR)
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'empleados.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'empleados.empleados.settings')
 
 application = get_wsgi_application()
