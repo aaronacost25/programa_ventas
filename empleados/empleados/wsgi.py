48fb41feaf3cfda468a4dd3b22f8b9b7d8423414
Aaron <acostaaaron@iresm.edu.ar>
2026-03-27 18:27:16 -0300

    correcion del path

--- empleados/empleados/wsgi.py
+++ empleados/empleados/wsgi.py
@@ -8,8 +8,12 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+import sys
+
+sys.path.append('/opt/render/project/src')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'empleados.empleados.settings')
 
