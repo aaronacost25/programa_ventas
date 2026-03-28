4cedb8d296846aee5ff771c0080e0a633de5be6d
Aaron <acostaaaron@iresm.edu.ar>
2026-03-27 22:53:37 -0300

    solucionar error

--- empleados/empleados/urls.py
+++ empleados/empleados/urls.py
@@ -20,6 +20,7 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
+    path('', include('applications.home.urls')),
     path('admin/', admin.site.urls),
     path('productos/', include('applications.productos.urls')),
     path('ventas/', include('applications.ventas.urls')),
